b2b44aae976b7761f1a9543d5a12c8df51e8a410
Mou95 <mauro.carlin3@gmail.com>
2017-02-01 17:37:08 +0100

    modifica mazzi

--- php/Page/Deck.php
+++ php/Page/Deck.php
@@ -3,7 +3,17 @@
 
     include_once "Page.php";
 
+    use \php\Database\Database;
+    use \php\Database\MySQLConnection\MySQLConnection;
+
     class Deck implements Page {
+
+        private $db;
+
+    	public function __construct() {
+    		$this->db = new Database(new MySQLConnection());
+    	}
+
         public function header() {
             $head = file_get_contents("html/header.html");
 
@@ -23,15 +33,77 @@
 
             echo $head;
         }
+
     	public function content() {
-            $content=file_get_contents("html/mazzi.html");
+            $contenuto=file_get_contents("html/mazzi.html");
 
-            //$content=str_replace(":tabellamazzi",funzione per trovare mazzi,$content);
+            if(isset($_GET['nome'])) {
+    			$contenuto = str_replace(':nomeMazzo:',
+    			'<input type="text" name="nome" value="'.$_GET['nome'].'"/>', $contenuto);
+    		}
+    		else {
+    			$contenuto = str_replace(':nomeMazzo:',
+    			'<input type="text" name="nome" />', $contenuto);
+    		}
 
-            echo $content;
+            if(isset($_GET['classe'])) {
+    			$contenuto = str_replace('<option>'.$_GET['classe'].'</option>',
+    			'<option selected>'.$_GET['classe'].'</option>', $contenuto);
+    		}
+
+            if(isset($_GET['costoMax'])) {
+                $contenuto= str_replace(':costoMax:',
+                '<input type="number" min="0" max="100000" name="costoMax" value="'.$_GET['costoMax'].'"/>', $contenuto);
+            }
+            else {
+                $contenuto= str_replace(':costoMax:',
+                '<input type="number" min="0" max="100000" name="costoMax" />', $contenuto);
+            }
+
+            if(isset($_GET['costoMin'])) {
+                $contenuto= str_replace(':costoMin:',
+                '<input type="number" min="0" max="100000" name="costoMin" value="'.$_GET['costoMin'].'"/>', $contenuto);
+            }
+            else {
+                $contenuto= str_replace(':costoMin:',
+                '<input type="number" min="0" max="100000" name="costoMin" />', $contenuto);
+            }
+
+            $contenuto=str_replace(':tabellaMazzi:',$this->mostraDeck(),$contenuto);
+
+            echo $contenuto;
         }
+
     	public function footer() {
 
+        }
+
+        public function mostraDeck() {
+            $query='SELECT D.deck_id, D.name, H.name, sum(R.r_craft) as Costo, D.likes, D.creation_date';
+            $query.='FROM card as C join card_deck as CD on (C.card_id=CD.card_id) join deck as D on (CD.deck_id=D.deck_id)
+                    join hero as H on (D.hero_id=H.hero_id) join rarity as R on (R.name=C.rarity)';
+
+            if(isset($_GET['nome']))
+            $query.="WHERE D.name LIKE '%".$_GET['nome']."%' AND  H.name LIKE '%".$_GET['classe']."%' ";
+            $query.='GROUP BY D.deck_id';
+            $this->db->query($query);
+            $rs=$this->db->resultset($query);
+
+            $final="";
+            foreach ($rs as $row) {
+                if ($row['Costo']<$_GET['CostoMin'] && $row['Costo']>$_GET['CostoMax'])
+                {
+                    $final.=
+                    "<tr>
+                        <td>{$row['D.deck_id']}</td>
+                        <td class=\"{$row['H.name']}\">{$row['H.name']}</td>
+                        <td>{$row['Costo']}</td>
+                        <td>{$row['D.likes']}</td>
+                        <td>{$row['D.creation_date']}</td>
+                    </tr>";
+                }
+            }
+            return $final;
         }
 
     }
