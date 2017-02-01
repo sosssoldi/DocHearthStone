814d042cc1dc9e8b835e5fd5506bfd5d432ee5d5
mbonolo <bonolomarco@gmail.com>
2017-02-02 00:03:55 +0100

    Add files via upload

---
+++ sql/populateDB/insert_guide.sql
@@ -0,0 +1,6 @@
+insert into guide values (1,"Guida1","NULL",0,"HERO_08","TiNtO");
+insert into guide values (2,"Guida2","NULL",0,"HERO_08","TiNtO");
+insert into guide values (3,"Guida3","NULL",0,"HERO_08","TiNtO");
+insert into guide values (4,"Guida4","NULL",0,"HERO_08","TiNtO");
+insert into guide values (5,"Guida5","NULL",0,"HERO_08","TiNtO");
+insert into guide values (6,"Guida6","NULL",0,"HERO_08","TiNtO");
