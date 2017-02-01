97d61e0c91d54e662a7a52e60e9a8382001678af
ntintorri <tintorri.nicola@gmail.com>
2017-02-01 18:58:48 +0100

    Aggiornato Card.php

--- php/Page/Card.php
+++ php/Page/Card.php
@@ -9,10 +9,10 @@ use \php\Database\MySQLConnection\MySQLConnection;
 
 class Card implements Page {
 
-	private $db;
+	private $db = null;
 
 	public function __construct() {
-		$db = new Database(new MySQLConnection());
+		$this->db = new Database(new MySQLConnection());
 	}
 	
 	public function header() {
@@ -84,6 +84,29 @@ class Card implements Page {
 		}
 		
 		//prendo tutte le carte
+		$query = $this->generaQuery();
+		$this->db->query($query);
+		$rs = $this->db->resultset();
+		
+		$riga = '';
+		
+		foreach($rs as $row) {
+			$riga .= '<tr>';
+			$riga .= '<td><span class="'.$row['rarity'].'">'.$row['name'].'</span><span class="descrizione">'.$row['description'].'</span></td>';
+			$riga .= '<td>'.$row['c_type'].'</td>';
+			
+			if($row['numClassi'] == 9)
+				$riga .= '<td>Neutrale</td>';
+			else
+				$riga .= '<td>'.$this->stampaClassi($row['name']).'</td>';
+			
+			$riga .= '<td class="mana">'.$row['mana'].'</td>';
+			$riga .= '<td class="attacco">'.$row['attack'].'</td>';
+			$riga .= '<td class="vita">'.$row['health'].'</td>';
+			$riga .= '</tr>';
+		}
+		
+		$contenuto = str_replace(':corpoTabella:', $riga, $contenuto);
 		
 		//content del documento
 		echo $contenuto;
@@ -91,6 +114,91 @@ class Card implements Page {
 	
 	public function footer() {
 		
+	}
+	
+	private function generaQuery() {
+		
+		$query = 'SELECT card.name, rarity, c_type, mana, attack, health, description, COUNT(card.name) as numClassi FROM hero, card, hero_card WHERE hero.hero_id = hero_card.hero_id AND card.card_id = hero_card.card_id';
+		
+		//nome carta
+		if(isset($_GET['nome']) AND $_GET['nome'] != '')
+			$query .= ' AND name = "'.$_GET['nome'].'"';
+
+		//costo in mana della carta
+		if(isset($_GET['costo']) AND $_GET['costo'] != '')
+			if($_GET['costo'] == '7+')
+				$query .= ' AND mana >= 7';
+			else
+				$query .= ' AND mana = '.$_GET['costo'];
+		
+		//avventura/espansione
+		if(isset($_GET['avventura']) AND $_GET['avventura'] != '') {
+			
+			if($_GET['avventura'] == "Karazhan")
+				$avventura='KARA';
+			
+			if($_GET['avventura'] == "Massiccio Roccianera")
+				$avventura='BRM';
+			
+			if($_GET['avventura'] == "Lega degli Esploratori")
+				$avventura='LOE';
+			
+			if($_GET['avventura'] == "Naxxramas")
+				$avventura='NAXX';
+			
+			if($_GET['avventura'] == "Goblin vs Golem")
+				$avventura='GVG';
+			
+			if($_GET['avventura'] == "Sussurro degli Dei Antichi")
+				$avventura='OG';
+			
+			if($_GET['avventura'] == "I bassifondi di Meccania")
+				$avventura='GANGS';
+			
+			if($_GET['avventura'] == "Il Gran Torneo")
+				$avventura='TGT';
+
+			$query .= ' AND (adventure_name = "'.$avventura.'" OR expansion_name = "'.$avventura.'")';
+		}
+		
+		//rarita
+		if(isset($_GET['rarita']) AND $_GET['rarita'] != '')
+			$query .= ' AND rarity = "'.$_GET['rarita'].'"';
+		
+		//tipo
+		if(isset($_GET['tipo']) AND $_GET['tipo'] != '')
+			$query .= ' AND c_type = "'.$_GET['tipo'].'"';
+		
+		//classe
+		if(isset($_GET['classe']) AND $_GET['classe'] != '')
+			$query .= ' AND type = "'.$_GET['classe'].'"';
+		
+		$query .= ' GROUP BY card.name, c_type, mana, attack, health;';
+		
+		return $query;
+	}
+	
+	private function stampaClassi($nomeCarta) {
+		
+		$elencoClassi = '';
+		
+		$query = 'SELECT type FROM hero, card, hero_card WHERE hero.hero_id = hero_card.hero_id AND card.card_id = hero_card.card_id AND card.name = "'.$nomeCarta.'"';
+
+		$this->db->query($query);
+		$rs = $this->db->resultset();
+		
+		if($this->db->rowCount($rs) == 9)
+			$elencoClassi .= 'Neutrale';
+		else {
+			foreach($rs as $row) {
+				if($elencoClassi == '')
+					$elencoClassi .= $row['type'];
+				else
+					$elencoClassi .= ', '.$row['type'];
+			}
+		}
+		
+		return $elencoClassi;
 	}
 }
 ?>
