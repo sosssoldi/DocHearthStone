6cde706c2f75ca9176d63e7092f1c727991a8bfe
mbonolo <bonolomarco@gmail.com>
2017-02-01 17:46:56 +0100

    Add files via upload

--- sql/populateDB/insert_hero_power.sql
+++ sql/populateDB/insert_hero_power.sql
@@ -1,10 +1,10 @@
 -- POPULATE TABLE:		HERO_POWER
-insert into hero_power values ('CS2_034', 'Detonazione di Fuoco', '<b>Potere Eroe</b>\nInfligge 1 danno.', 'CS2_034.png');
-insert into hero_power values ('DS1h_292', 'Tiro Stabilizzato', '<b>Potere Eroe</b>\nInfligge 2 danni all\'eroe nemico.', 'DS1h_292.png');
-insert into hero_power values ('CS2_101', 'Rinforzo', '<b>Potere Eroe</b>\nEvoca una Recluta Mano d\'Argento 1/1.', 'CS2_101.png');
-insert into hero_power values ('CS2_102', 'Potenziamento Armatura', '<b>Potere Eroe</b>\nOttiene 2 Armatura.', 'CS2_102.png');
-insert into hero_power values ('CS2_017', 'Mutaforma', '<b>Potere Eroe</b>\n+1 Attacco per questo turno e 1 Armatura.', 'CS2_017.png');
-insert into hero_power values ('CS2_056', 'Conversione Vitale', '<b>Potere Eroe</b>\nPesca una carta e subisce 2 danni.', 'CS2_056.png');
-insert into hero_power values ('CS2_049', 'Evocazione Totemica', '<b>Potere Eroe</b>\nEvoca un Totem casuale.', 'CS2_049.png');
-insert into hero_power values ('CS1h_001', 'Cura Inferiore', '<b>Potere Eroe</b>\nRigenera 2 Salute.', 'CS1h_001.png');
-insert into hero_power values ('CS2_083b', 'Maestria nei Pugnali', '<b>Potere Eroe</b>\nEquipaggia un pugnale 1/2.', 'CS2_083b.png');
+insert into hero_power values ('CS2_034', 'Detonazione di Fuoco', '<b>Potere Eroe</b>\nInfligge 1 danno.', 'mage.png');
+insert into hero_power values ('DS1h_292', 'Tiro Stabilizzato', '<b>Potere Eroe</b>\nInfligge 2 danni all\'eroe nemico.', 'hunter.png');
+insert into hero_power values ('CS2_101', 'Rinforzo', '<b>Potere Eroe</b>\nEvoca una Recluta Mano d\'Argento 1/1.', 'paladin.png');
+insert into hero_power values ('CS2_102', 'Potenziamento Armatura', '<b>Potere Eroe</b>\nOttiene 2 Armatura.', 'warrior.png');
+insert into hero_power values ('CS2_017', 'Mutaforma', '<b>Potere Eroe</b>\n+1 Attacco per questo turno e 1 Armatura.', 'druid.png');
+insert into hero_power values ('CS2_056', 'Conversione Vitale', '<b>Potere Eroe</b>\nPesca una carta e subisce 2 danni.', 'warlock.png');
+insert into hero_power values ('CS2_049', 'Evocazione Totemica', '<b>Potere Eroe</b>\nEvoca un Totem casuale.', 'shaman.png');
+insert into hero_power values ('CS1h_001', 'Cura Inferiore', '<b>Potere Eroe</b>\nRigenera 2 Salute.', 'priest.png');
+insert into hero_power values ('CS2_083b', 'Maestria nei Pugnali', '<b>Potere Eroe</b>\nEquipaggia un pugnale 1/2.', 'rogue.png');
