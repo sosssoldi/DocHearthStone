74e26e0c60d0ea38e13665815a7271c4becaa6ec
ntintorri <tintorri.nicola@gmail.com>
2017-02-02 02:14:41 +0100

    Aggiunto avventure.php

--- avventure.php
+++ avventure.php
@@ -1,26 +1,11 @@
-<!DOCTYPE html>
-<html lang="it">
-	<head>
-		<meta charset="utf-8" />
-		<title>Avventure - DocHearthStone</title>
-		<meta name="title" content="Avventure - DocHearthStone" />
-		<meta name="description" content="La descrizione delle carte e degli eroi che si troveranno acquistando ciascuna avventura." />
-		<meta name="keyword" content="DocHearthStone, avventura, la maledizione di Naxxramas, naxxramas, Massiccio Roccianera, roccianera, Lega degli Esploratori, Esploratori, Una notte a Karazhan, Karazhan" />
-    	<link rel="stylesheet" href="css/avventure.css" type="text/css" />
-        <link rel="stylesheet" href="css/header.css" type="text/css" />
-        <link rel="stylesheet" href="css/generale.css" type="text/css" />
-		<script src="../script/avventure.js"></script>
-	</head>
-    <body onload="init();">
-    <?php
-        include_once 'autoloader.php';
-        use \php\Page\Adventure;
-
-        $obj= new Adventure();
-
-        $obj->header();
-        $obj->content();
-        $obj->footer();
-    ?>
-    </body>
-</html>
+<?php
+	session_start();
+	include_once 'autoloader.php';
+	use \php\Page\Adventure;
+	echo file_get_contents("html/avventure_head.html");
+	$obj= new Adventure();
+	$obj->header();
+	$obj->content();
+	$obj->footer();
+	echo file_get_contents("html/chiudi.html");
+?>
