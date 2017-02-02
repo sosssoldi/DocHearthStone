0a8a6335bbe37941681e0bdd1ecffa81687b29c4
sosssoldi <angus_67@live.it>
2017-02-02 01:15:32 +0100

    lo spettacolo delle 1.15

--- php/Page/Homepage.php
+++ php/Page/Homepage.php
@@ -21,10 +21,17 @@ class Homepage implements Page {
         if(isset($_SESSION["username"])) {
             $head = str_replace(":login:",
             '<li><a href="user.php">'.$_SESSION["username"].'</a></li>', $head);
+            $head = str_replace(":utente:",
+                '<div id="boxutente">
+                    <span>'.$_SESSION["username"].'</span>
+                    <a href="logout.php"><button>Logout</button></a>
+                </div>'
+            ,$head);
         }
         else {
             $head = str_replace(":login:",
             '<li lang="en"><a href="login.php">LOGIN</a></li>', $head);
+            $head = str_replace(":utente:",'',$head);
         }
 
         //here
