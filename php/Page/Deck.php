ab46fc7383e9ffdc8464955758757bc0be73a3ee
mbonolo <bonolomarco@gmail.com>
2017-02-04 05:47:18 +0100

    Add files via upload

--- php/Page/Deck.php
+++ php/Page/Deck.php
@@ -9,6 +9,24 @@
     class Deck implements Page {
 
         private $db;
+        private $errore = array(
+                            "",
+                            "Errore: Sono stati inseriti dei caratteri non corretti nel nome",
+                            "Errore: Questa classe eroe non esiste",
+                            "Errore: Il nome non può contenere caratteri sbagliati e la classe eroe deve essere una delle 9 selezionabili",
+                            "Errore: Nel campo del costo minimo deve essere inserito un numero (maggiore di zero)",
+                            "Errore: Il nome non può contenere caratteri sbagliati e il costo minimo deve essere un numero (maggiore di zero)",
+                            "Errore: La classe eroe deve essere una tra quelle selezionabili e il costo minimo deve essere un numero (maggiore di zero)",
+                            "Errore: Nome, Classe e costo minimo contengono dei dati sbagliati",
+                            "Errore: Nel campo del costo massimo deve essere inserito un numero (maggiore di zero e maggiore del costo minimo)",
+                            "Errore: Il nome contiene caratteri sbagliati e il costo massimo deve essere un numero (maggiore di zero e del costo minimo)",
+                            "Errore: La classe eroe specificata non esiste e il costo massimo deve &egrave; sbagliato, deve essere un numero",
+                            "Errore: Nome, Classe e Costo massimo sono sbagliati",
+                            "Errore: I costi sono sbagliati, devono essere dei numeri e il costo massimo deve essere maggiore del costo minimo",
+                            "Errore: Nome, costo minimo e costo massimo sono sbagliati",
+                            "Errore: Classe, costo minimo e costo massimo sono sbagliati",
+                            "Errore: Nome, Classe, costo minimo e costo massimo sono sbagliati. Ricontrolla i dati inseriti"
+                            );
 
     	public function __construct() {
     		$this->db = new Database(new MySQLConnection());
@@ -43,8 +61,15 @@
 
     	public function content() {
             $contenuto=file_get_contents("html/mazzi.html");
-
+            $err = 0;
             if(isset($_GET['nome'])) {
+                if(strstr($_GET['nome'], '>') || strstr($_GET['nome'], '<') || strstr($_GET['nome'], "'"))
+                    $err = 1;
+
+                $_GET['nome'] = str_replace("<", "&lt;", $_GET["nome"]);
+                $_GET['nome'] = str_replace(">", "&gt;", $_GET["nome"]);
+                $_GET['nome'] = str_replace("'", "\'", $_GET["nome"]);
+
     			$contenuto = str_replace(':nomeMazzo:',
     			'<input type="text" name="nome" value="'.$_GET['nome'].'"/>', $contenuto);
     		}
@@ -53,12 +78,21 @@
     			'<input type="text" name="nome" />', $contenuto);
     		}
 
+            $heroes = array("","Mago","Sacerdote","Druido","Cacciatore","Guerriero","Paladino","Stregone","Sciamano","Ladro");
+            $found = 0;
             if(isset($_GET['classe'])) {
+                foreach ($heroes as $h)
+                    if($h == $_GET["classe"])
+                        $found = 1;
+                if($found == 0)
+                    $err += 2;
     			$contenuto = str_replace('<option>'.$_GET['classe'].'</option>',
     			'<option selected>'.$_GET['classe'].'</option>', $contenuto);
     		}
 
-            if(isset($_GET['costoMax'])) {
+            if(isset($_GET['costoMax']) && $_GET['costoMax']!="") {
+                if(!is_numeric($_GET["costoMax"]) || $_GET["costoMax"] < 0)
+                    $err += 4;
                 $contenuto= str_replace(':costoMax:',
                 '<input type="number" min="0" max="100000" name="costoMax" value="'.$_GET['costoMax'].'"/>', $contenuto);
             }
@@ -67,7 +101,14 @@
                 '<input type="number" min="0" max="100000" name="costoMax" />', $contenuto);
             }
 
-            if(isset($_GET['costoMin'])) {
+            if(isset($_GET['costoMin']) && $_GET['costoMin']!="") {
+                if(isset($_GET["costoMax"]) && $_GET["costoMax"]!="") {
+                    if(!is_numeric($_GET["costoMin"]) || $_GET["costoMin"] < $_GET["costoMax"])
+                        $err += 8;
+                } else {
+                    if(!is_numeric($_GET["costoMin"]) || $_GET["costoMin"] < 0)
+                        $err += 8;
+                }
                 $contenuto= str_replace(':costoMin:',
                 '<input type="number" min="0" max="100000" name="costoMin" value="'.$_GET['costoMin'].'"/>', $contenuto);
             }
@@ -76,8 +117,11 @@
                 '<input type="number" min="0" max="100000" name="costoMin" />', $contenuto);
             }
 
-            $contenuto=str_replace(':tabellamazzi:',$this->mostraDeck(),$contenuto);
-
+            $contenuto = str_replace(":errore:","<p class=\"errore\">".$this->errore[$err]."</p>",$contenuto);
+            if($err == 0)
+                $contenuto=str_replace(':tabellamazzi:',$this->mostraDeck(),$contenuto);
+            else
+                $contenuto=str_replace(':tabellamazzi:',"",$contenuto);
             echo $contenuto;
         }
 
@@ -152,30 +196,30 @@
 
             return $final;
         }
-		
+
 		public function eliminaMazzo($id) {
-			
+
 			$query = 'SELECT user_name FROM deck WHERE user_name = "'.$_SESSION['username'].'" AND deck_id = '.$id;
 			$this->db->query($query);
 			$rs = $this->db->resultset();
-			
+
 			//Se la query ha una riga allora l'username ha anche creato la guida
 			if($this->db->rowCount() > 0) {
 				//cancello i like in deck_like
 				$query = 'DELETE FROM deck_like WHERE deck_id = '.$id;
 				$this->db->query($query);
 				$this->db->execute();
-				
+
 				//cancello i suggest legati al mazzo
 				$query = 'DELETE FROM suggest WHERE deck_id = '.$id;
 				$this->db->query($query);
 				$this->db->execute();
-				
+
 				//cancello le carte legate al deck da deck_card
 				$query = 'DELETE FROM card_deck WHERE deck_id = '.$id;
 				$this->db->query($query);
 				$this->db->execute();
-				
+
 				//cancello il deck
 				$query = 'DELETE FROM deck WHERE deck_id = '.$id;
 				$this->db->query($query);
