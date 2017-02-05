7ed5ddeb37475d32deb2fb4b23cc8436cdf416c0
ntintorri <tintorri.nicola@gmail.com>
2017-02-05 19:18:10 +0100

    eliminaMazzo.php

--- eliminaMazzo.php
+++ eliminaMazzo.php
@@ -9,19 +9,17 @@
 	*/
 
 	//controllo se admin e se provengo da una delle 2 pagine in questione
-	if(isset($_GET['mazzo']) && $_SESSION['username'] == 'admin' && strstr($_SERVER["HTTP_REFERER"],"mazzi.php"))
-
+	if(isset($_GET['mazzo']) && isset($_SESSION['username']) && $_SESSION['username'] == 'admin')
 		$status=1;
-		//controllo se utente loggato e provengo da form valido
+	//controllo se utente loggato e provengo da form valido
 	else if(isset($_SESSION['username']) && isset($_GET['mazzo']) && $_GET['mazzo'] != "")
 		$status=2;
 	else
 		header("Location: index.php");
 
-	if($status==1 || $status==2) {
-		$obj = new Deck();
-		$obj->eliminaMazzo($_GET['mazzo'],$status);
-	}
+	$obj = new Deck();
+	$obj->eliminaMazzo($_GET['mazzo'],$status);
+	
 	if($status==1)
 		header("Location: mazzi.php");
 	else
